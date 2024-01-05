f5eef848be4416c8a73af5d9a59449fbc515d831
mega-hedge <110251934+mega-hedge@users.noreply.github.com>
2024-01-05 16:49:41 +0000

    33333

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-import aiogram
+from aiogram import Bot, executor, Dispatcher, types
+from config import bot_token
+import random as rn
 
-1111111
+bot = Bot(bot_token)
+dp = Dispatcher(bot = bot)
+
+@dp.message_handler( commands = ['start'] )
+async def start(msg: types.Message) -> None:
+    await msg.answer('Hello. This is a bot for generating random text.')
+
+@dp.message_handler( commands = ['random_word'] )
+async def echo(msg: types.Message) -> None:
+    with open('Text Languages/ru_list.txt', encoding='utf-8') as f:
+        await msg.answer(rn.choice(f.read().splitlines()).lower())
+    
+if __name__ == "__main__":
+    executor.start_polling(dp)
